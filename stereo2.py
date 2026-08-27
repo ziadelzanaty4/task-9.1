d5d17df35bbff6b4a99dd38ee4237e0e49cc013a
ziadelzanaty4 <ziadelzanaty4@gmail.com>
2026-08-27 22:59:58 +0300

    added the same code for the second folder while changing the parameters

--- stereo2.py
+++ stereo2.py
@@ -2,14 +2,14 @@ import cv2
 import numpy as np
 
 
-fx = 3979.911
-baseline = 193.001 / 10.0   # convert mm to cm
-doffs = 124.343              
+fx = 6338.47
+baseline = 171.548/ 10.0   # convert mm to cm
+doffs = 479.489              
 
-left_img = cv2.imread('im0/im0.png', 0)
-right_img = cv2.imread('im0/im1.png', 0)
+left_img = cv2.imread('img1/im0.png', 0)
+right_img = cv2.imread('img1/im1.png', 0)
 
-matcher = cv2.StereoBM_create(numDisparities=272, blockSize=15)
+matcher = cv2.StereoBM_create(numDisparities=400, blockSize=15)
 
 
 disparity = matcher.compute(left_img, right_img)
@@ -20,9 +20,9 @@ disparity = disparity.astype(np.float32) / 16.0
 disp_view = cv2.normalize(disparity, None, 0, 255, cv2.NORM_MINMAX)
 disp_view = np.uint8(disp_view)
 
-cv2.imwrite('im0/disparity_gray.png', disp_view)
+cv2.imwrite('img1/disparity_gray.png', disp_view)
 disp_color = cv2.applyColorMap(disp_view, cv2.COLORMAP_JET)
-cv2.imwrite('im0/disparity_color.png', disp_color)
+cv2.imwrite('img1/disparity_color.png', disp_color)
 
 def get_depth(disp_map, x, y, fx, baseline, doffs):
     d = disp_map[y, x]
@@ -30,8 +30,8 @@ def get_depth(disp_map, x, y, fx, baseline, doffs):
         return None
     return (fx * baseline) / (d + doffs)
 
-x = 300
-y = 100
+x = 1450
+y = 900
 depth = get_depth(disparity, x, y, fx, baseline, doffs)
 
 if depth is not None:
